0ce42868e59d1b87417ff281d036938085ece511
Arrington Perry <perryceo21@hotmail.com>
2023-12-23 20:45:07 -0500

    added code to switch from splash to main login screen

--- app/src/main/java/com/example/smartfundsbank/SplashActivity.kt
+++ app/src/main/java/com/example/smartfundsbank/SplashActivity.kt
@@ -1,11 +1,22 @@
 package com.example.smartfundsbank
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        val handler = Handler()
+        handler.postDelayed({splashScreen()
+        }, 3500)
+    }
+
+    private fun splashScreen(){
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
     }
 }
